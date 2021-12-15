3e8ae25e0e2bbc0839a3bc1062030f8879a3f263
Hoa Le <Hoa_le9485@yahoo.com>
2021-12-15 01:06:54 -0500

    commit tree 202601124c93c1ca58f56c1baf94c8029a639963

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :name
+  
+    def initialize(word)
+      @name = word 
+    end   
+  
+    def match(array)
+      array.select {|x| x.split("").sort == @name.split("").sort}
+    end   
+  #Returns a new hash consisting of entries for which the block returns true.
+  end 
